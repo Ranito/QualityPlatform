07166dfd170e4506febcff34adbb17d579ec91db
Pedro Ranito <pedro.ranito@verifile.co.uk>
2025-12-19 18:03:59 +0000

    Add project files.

---
+++ TaxCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TaxCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tax Calculator");
+        }
+    }
+}
